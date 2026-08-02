cbdf7c00ab214d37e95acd922c1a1d2fc0c480c0
Aman Gupta <amangupta052@gmail.com>
2026-08-02 20:55:34 +0800

    DeepseekV4 MTP + DSpark (#25784)

--- src/llama-arch.cpp
+++ src/llama-arch.cpp
@@ -973,6 +973,7 @@ bool llm_arch_is_hybrid(const llm_arch & arch) {
         case LLM_ARCH_KIMI_LINEAR:
         case LLM_ARCH_QWEN35:
         case LLM_ARCH_QWEN35MOE:
+        case LLM_ARCH_DEEPSEEK4:
             return true;
         default:
             return false;
@@ -995,6 +996,7 @@ bool llm_arch_supports_rs_rollback(const llm_arch & arch) {
     switch (arch) {
         case LLM_ARCH_QWEN35:
         case LLM_ARCH_QWEN35MOE:
+        case LLM_ARCH_DEEPSEEK4:
             return true;
         default:
             return false;
